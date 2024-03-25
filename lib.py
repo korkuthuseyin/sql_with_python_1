20fcf88aba5caf0d289a5c5308eb3698e1ca0eca
korkuthuseyin <korkuth21@itu.edu.tr>
2024-03-25 05:24:42 +0300

    added functions

---
+++ lib.py
@@ -0,0 +1,35 @@
+import psycopg2
+
+def connect_db():
+    conn = psycopg2.connect(
+        dbname='korkut',
+        user='postgres',
+        password='yzv322e',
+        host='127.0.0.1',
+        port='5432'
+    )
+    cur = conn.cursor()
+    return conn, cur
+
+def close_db(conn, cur):
+    conn.commit()
+    cur.close()
+    conn.close()
+
+def create_table(tablename):
+    conn, cur = connect_db()
+    cur.execute(f'CREATE TABLE {tablename} (id SERIAL PRIMARY KEY, name VARCHAR(50), barcode VARCHAR(50))')
+    close_db(conn, cur)
+    
+def insert_data(tablename, name, barcode):
+    conn, cur = connect_db()
+    # take biggest id and increment it by 1
+    cur.execute(f"SELECT MAX(id) FROM {tablename}")
+    last_id = cur.fetchone()[0]
+    if last_id is not None:
+        next_id = last_id + 1
+    else:
+        next_id = 1
+    
+    cur.execute(f"INSERT INTO {tablename} (id, name, barcode) VALUES ('{next_id}', '{name}', '{barcode}')")
+    close_db(conn, cur)
---
+++ main.py
@@ -0,0 +1,4 @@
+import psycopg2
+import lib
+
+lib.insert_data('products', 'product2', '351234')
